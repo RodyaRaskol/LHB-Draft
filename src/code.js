7ee8af368ada92ef4ab208717a3b07bf329834c0
RodyaRaskol <mark@srigala.asia>
2022-02-17 18:38:28 +0800

    Moved Images

--- src/code.js
+++ src/code.js
@@ -6,9 +6,10 @@ function BuildMainNaviBar(myPage) {
     //Build Left Side
     bar += '<div class="nav-left-side">';
         bar += '<div id="jsNaviBar_index" class="nav-link-wrapper"><a href="index.html">Home</a></div>';
+        bar += '<div id="jsNaviBar_work" class="nav-link-wrapper"><a href="work.html">Excel</a></div>';
         bar += '<div id="jsNaviBar_work" class="nav-link-wrapper"><a href="work.html">Work</a></div>';
         bar += '<div id="jsNaviBar_knowledge" class="nav-link-wrapper"><a href="knowledge.html">Knowledge</a></div>';
-        bar += '<div id="jsNaviBar_food" class="nav-link-wrapper"><a href="food.html">Food</a></div>';
+        //bar += '<div id="jsNaviBar_food" class="nav-link-wrapper"><a href="food.html">Food</a></div>';
        // bar += '<div id="jsNaviBar_play"class="nav-link-wrapper"><a href="play.html">Play</a></div>';
         bar += '<div id="jsNaviBar_stalls"class="nav-link-wrapper"><a href="stalls.html">Stalls</a></div>';
         bar += '<div id="jsNaviBar_about" class="nav-link-wrapper"><a href="about.html">About</a></div>';
@@ -30,11 +31,42 @@ function BuildMainNaviBar(myPage) {
     document.getElementById(myPage).className +="active-nav-link";
   
 };
-function BuildFoodNaviBar(mySubPage) {
+function BuildSubNaviBar(mySubPage) {
 
     var mySubMenu;
    /* "ingredients": { id: "fmIngredients", page: "ingredients.html",display:"Ingredients" }*/
-            var foodmenus = [
+            var FoodSubs = [
+                ["fmHome","food.html","Food Home"],
+                [ "fmIngredients","ingredients.html","Ingredients"],
+               ["fmfoodtips", "foodtips.html","Tips"]
+                ];
+    mySubMenu=FoodSubs
+    let bar = '';
+    //Open Navigation
+    bar += '<div class="subnav-wrapper">';
+    
+    //Build Left Side
+    bar += '<div class="subnav-left-side">';
+        for (var i = 0; i < submenus.length; i++) {
+               mySubMenu= '<div id="'+submenus[i][0]+'" class="subnav-link-wrapper"><a href="'+submenus[i][1]+'">'+submenus[i][2]+'</a></div>';
+                bar += mySubMenu;
+            }
+    bar += '</div>';//Close Left
+
+    bar += '</div>';//Close Wrapper
+
+    $("#sub-bar").html(bar);
+ 
+
+    //Add Active page marker
+    document.getElementById(mySubPage).className +="active-subnav-link";
+  
+};
+function BuildExcelNaviBar(mySubPage) {
+
+    var mySubMenu;
+   /* "ingredients": { id: "fmIngredients", page: "ingredients.html",display:"Ingredients" }*/
+            var submenus = [
                 ["fmHome","food.html","Food Home"],
                 [ "fmIngredients","ingredients.html","Ingredients"],
                ["fmfoodtips", "foodtips.html","Tips"]
@@ -45,8 +77,8 @@ function BuildFoodNaviBar(mySubPage) {
     
     //Build Left Side
     bar += '<div class="subnav-left-side">';
-        for (var i = 0; i < foodmenus.length; i++) {
-               mySubMenu= '<div id="'+foodmenus[i][0]+'" class="subnav-link-wrapper"><a href="'+foodmenus[i][1]+'">'+foodmenus[i][2]+'</a></div>';
+        for (var i = 0; i < submenus.length; i++) {
+               mySubMenu= '<div id="'+submenus[i][0]+'" class="subnav-link-wrapper"><a href="'+submenus[i][1]+'">'+submenus[i][2]+'</a></div>';
                 bar += mySubMenu;
             }
     bar += '</div>';//Close Left
